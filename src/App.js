8163b2791ad433aab90bf99f0a864e71a9470386
Min Khant Hein <mk.hein@mit.com.mm>
2023-03-27 23:05:45 +0630

    50mins

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import About from "./components/pages/About";
+import Contact from "./components/pages/Contact";
+import Home from "./components/pages/Home";
+
+import "../node_modules/bootstrap/dist/css/bootstrap.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import PageNotFound from "./components/pages/PageNotFound";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home/>}/>
+          <Route exact path="/about" element={<About/>}/>
+          <Route path="/contact" element={<Contact/>}/>
+          <Route path="*" element={<PageNotFound/>}/>
+        </Routes>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/PageNotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageNotFound = () => {
+    return ( 
+        <div>
+            <h1 className='display-1 text-center py-5 my-5'>Page Not Found</h1>
+        </div>
+     );
+}
+ 
+export default PageNotFound;
